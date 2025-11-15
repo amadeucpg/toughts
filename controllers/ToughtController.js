1b369b139e8d63763e87b81cb2242dff855bab32
Amadeu <amadeucpg@gmail.com>
2025-11-14 21:10:36 -0300

    update e show

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -1,10 +1,49 @@
+const { raw } = require('express')
 const Tought = require('../models/Tought')
 const User = require('../models/User')
 
+const { Op } = require('sequelize')
+
 module.exports = class ToughtController {
-    static async showToughts(req, res) {
-        res.render('toughts/home')
+   static showToughts(req, res) {
+    console.log(req.query)
+
+    // check if user is searching
+    let search = ''
+
+    if (req.query.search) {
+      search = req.query.search
     }
+
+    // order results, newest first
+    let order = 'DESC'
+
+    if (req.query.order === 'old') {
+      order = 'ASC'
+    } else {
+      order = 'DESC'
+    }
+
+    Tought.findAll({
+      include: User,
+      where: {
+        title: { [Op.like]: `%${search}%` },
+      },
+      order: [['createdAt', order]],
+    })
+      .then((data) => {
+        let toughtsQty = data.length
+
+        if (toughtsQty === 0) {
+          toughtsQty = false
+        }
+
+        const toughts = data.map((result) => result.get({ plain: true }))
+
+        res.render('toughts/home', { toughts, toughtsQty, search })
+      })
+      .catch((err) => console.log(err))
+  }
 
     static async dashboard(req, res){
         const userid = req.session.userid
@@ -71,6 +110,34 @@ module.exports = class ToughtController {
             console.log('Aconteceu um erro:' + error)
         }
     }
+
+    static updateTought(req, res) {
+    const id = req.params.id
+
+    Tought.findOne({ where: { id: id }, raw: true })
+      .then((tought) => {
+        res.render('toughts/edit', { tought })
+      })
+      .catch((err) => console.log())
+  }
+
+  static updateToughtPost(req, res) {
+    const id = req.body.id
+
+    const tought = {
+      title: req.body.title,
+      description: req.body.description,
+    }
+
+    Tought.update(tought, { where: { id: id } })
+      .then(() => {
+        req.flash('message', 'Pensamento atualizado com sucesso!')
+        req.session.save(() => {
+          res.redirect('/toughts/dashboard')
+        })
+      })
+      .catch((err) => console.log())
+  }
 }
 
  
